17473faa9464b9b6906ba27a6b04258d6510f292
Satyendra <satyendra.bhati@girnarsoft.com>
2020-12-02 14:47:48 +0530

    Initial Commit

---
+++ Custom_Table/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom_Table
+//
+//  Created by Satyendra  Singh on 02/12/20.
+//  Copyright © 2020 Satyendra  Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
