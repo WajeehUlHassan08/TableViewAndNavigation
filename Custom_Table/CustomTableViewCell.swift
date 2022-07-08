241b995a95b131a7284ccf4839b7fc133b83975c
WajeehUlHassan08 <wajeehqadri@hotmail.com>
2022-07-08 06:00:30 +0100

    commit for project

---
+++ Custom_Table/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  Custom_Table
+//
+//  Created by Satyendra  Singh on 02/12/20.
+//  Copyright © 2020 Satyendra  Singh. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Custom_Table/SceondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SceondViewController.swift
+//  Custom_Table
+//
+//  Created by Satyendra  Singh on 02/01/21.
+//  Copyright © 2021 Satyendra  Singh. All rights reserved.
+//
+
+import UIKit
+
+class SceondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
